a52f0888b803fbf8a7297b2707c29e62c7153a2b
Rio Edwards <rioredwards@gmail.com>
2022-09-27 11:29:38 -0700

    Pass CI

--- fetch-utils.js
+++ fetch-utils.js
@@ -2,7 +2,6 @@ const SUPABASE_URL = 'https://brulbkgurviccbjegwdn.supabase.co';
 const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJydWxia2d1cnZpY2NiamVnd2RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM4ODE1OTcsImV4cCI6MTk3OTQ1NzU5N30.Gj7MdCMXmHCK96smfmJ3As3mqB_QPLujl1hFDOgR2w8';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-console.log(client);
 /* Auth related functions */
 
 export function getUser() {
